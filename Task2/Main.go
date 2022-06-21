84dfa5534a2f8c2840c2b34e335cceefbfd9d29c
YaroslavNykolyshyn <yaroslav.nyko@gmail.com>
2022-06-21 13:37:35 +0300

    commit task 2

--- Task2/Main.go
+++ Task2/Main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	input := "1 9 3 4 -5"
+	var myInt []int
+	min, max := findMinAndMax(myInt)
+	myStrSlice := strings.Split(input, " ")
+	for _, i := range myStrSlice {
+		myInt, _ := strconv.Atoi(i)
+		fmt.Println(myInt)
+	}
+	fmt.Println(myInt)
+
+	fmt.Println("Min: ", min)
+	fmt.Println("Max: ", max)
+}
+
+func findMinAndMax(myInt []int) (min int, max int) {
+	min = myInt[0]
+	max = myInt[0]
+	for _, value := range myInt {
+		if value < min {
+			min = value
+		}
+		if value > max {
+			max = value
+		}
+	}
+	return min, max
 }
